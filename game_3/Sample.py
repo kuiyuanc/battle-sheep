d87c8f722b21fb5761f4bc844f0cb72993fe8ad4
110550035 <kono.c@nycu.edu.com>
2024-04-08 20:05:32 +0800

    chore: Udpate dependencies

--- game_3/Sample.py
+++ game_3/Sample.py
@@ -5,10 +5,10 @@ import random
 '''
     選擇起始位置
     選擇範圍僅限場地邊緣(至少一個方向為牆)
-
+    
     return: init_pos
     init_pos=[x,y],代表起始位置
-
+    
 '''
 
 
@@ -23,16 +23,16 @@ def InitPos(mapStat):
 
 '''
     產出指令
-
-    input:
+    
+    input: 
     playerID: 你在此局遊戲中的角色(1~4)
-    mapStat : 棋盤狀態(list of list), 為 12*12矩陣,
+    mapStat : 棋盤狀態(list of list), 為 12*12矩陣, 
               0=可移動區域, -1=障礙, 1~4為玩家1~4佔領區域
     sheepStat : 羊群分布狀態, 範圍在0~16, 為 12*12矩陣
 
     return Step
     Step : 3 elements, [(x,y), m, dir]
-            x, y 表示要進行動作的座標
+            x, y 表示要進行動作的座標 
             m = 要切割成第二群的羊群數量
             dir = 移動方向(1~9),對應方向如下圖所示
             1 2 3
@@ -45,7 +45,7 @@ def GetStep(playerID, mapStat, sheepStat):
     step = [(0, 0), 0, 1]
     '''
     Write your code here
-
+    
     '''
     return step
 
