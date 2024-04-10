bcd09fdf785895cab0b9036e908467188db99c61
110550035 <kono.c@nycu.edu.com>
2024-04-10 23:27:05 +0800

    fix: Give winner bonus in terminal state only

--- game_1/Sample.py
+++ game_1/Sample.py
@@ -81,24 +81,27 @@ class MinMaxNode:
         return self._GetTeamScore() - sum(self._GetPlayerScore(player) for player in set(range(1, NUM_PLAYER + 1)) - self.teammate)
 
     def _GetTeamScoreWithWinnerBonus(self, *, grouped=False):
+        is_terminal = self.GetLegalStep()
         score = [self._GetPlayerScore(player) for player in range(1, NUM_PLAYER + 1)]
         team_score = sum(score[player] for player in self.teammate)
         is_winner = sum(score) - team_score < team_score if grouped else team_score == max(score)
-        return team_score + (self.WINNER_BONUS if is_winner else 0)
+        return team_score + (self.WINNER_BONUS if is_winner and is_terminal else 0)
 
     def _GetTeamScoreDifferenceWithWinnerBonus(self, *, grouped=False):
+        is_terminal = self.GetLegalStep()
         score = [self._GetPlayerScore(player) for player in range(1, NUM_PLAYER + 1)]
         team_score = sum(score[player] for player in self.teammate)
         opponent_score = sum(score) - team_score
         is_winner = opponent_score < team_score if grouped else team_score == max(score)
-        return team_score + (self.WINNER_BONUS if is_winner else 0) - opponent_score
+        return team_score + (self.WINNER_BONUS if is_winner and is_terminal else 0) - opponent_score
 
     def _GetTeamScoreWithWinnerBonusAndStupidPunish(self, *, grouped=False):
+        is_terminal = self.GetLegalStep()
         score = [self._GetPlayerScoreWithStupidPunish(player) for player in range(1, NUM_PLAYER + 1)]
         team_score = sum(score[player] for player in self.teammate)
         opponent_score = sum(score) - team_score
         is_winner = opponent_score < team_score if grouped else team_score == max(score)
-        return team_score + (self.WINNER_BONUS if is_winner else 0) - opponent_score
+        return team_score + (self.WINNER_BONUS if is_winner and is_terminal else 0) - opponent_score
 
     '''
 
