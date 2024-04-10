c65c30b85dbdb7d61cdfa83776ff767042bbdaba
110550035 <kono.c@nycu.edu.com>
2024-04-10 14:29:41 +0800

    feat: Running saved executable

--- run.bat
+++ run.bat
@@ -1,10 +1,12 @@
 @REM Put this file in root directory of this repo
 @REM This file is for fast running "after" running build.bat
 
-@REM Usage: ./run.bat <game-setting=1>
+@REM Usage: ./run.bat <game-setting=1> <executable-name-1=Model_1> <agent-to-replace-1=1>
+@REM To pass latter parameters, the former parameters should all be passed because they are positional arguments
 
 @REM Explanation:
-@REM 1. Run 'AI_game.exe' with assigned game setting ( default: 1 )
+@REM 1. Adjust 'input.txt' according to assigned agent
+@REM 2. Run 'AI_game.exe' with assigned game setting ( default: 1 )
 
 @cls
 @echo off
@@ -16,7 +18,24 @@ if "%1"=="" (
     set game_setting=%1
 )
 
+@REM Check if there is an assignment of executable or not
+if "%2"=="" (
+    set executable1=Model_1
+) else (
+    set executable1=%2
+)
+
+@REM Check if there is an assignment of agnet to be replaced with or not
+if "%3"=="" (
+    set agent1=1
+) else (
+    set agent1=%3
+)
+
 cd game_%game_setting%
+
+echo Pre-processing...
+py ../fix_input.py %agent1% %executable1%
 
 echo Running game...
 start AI_game.exe
