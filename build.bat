e4cbf1534c7811f69bc99714678e46b7fddea9b3
110550035 <kono.c@nycu.edu.com>
2024-04-10 14:33:51 +0800

    style: Unify parameter order

--- build.bat
+++ build.bat
@@ -1,7 +1,7 @@
 @REM Put this file in root directory of this repo
 @REM This file wraps your source code and run it
 
-@REM Usage: ./build.bat <executable-name-1=Model_1> <game-setting=1> <source-code-name-1=Sample> <agent-to-replace-1=1>
+@REM Usage: ./build.bat <game-setting=1> <executable-name-1=Model_1> <agent-to-replace-1=1> <source-code-name-1=Sample>
 @REM To pass latter parameters, the former parameters should all be passed because they are positional arguments
 
 @REM Explanation:
@@ -12,32 +12,32 @@
 @cls
 @echo off
 
-@REM Check if there is any assignment of wrapped executable name
-if "%1"=="" (
-    set executable1=Model_1
-) else (
-    set executable1=%1
-)
-
 @REM Check if there is an assignment of student ID or not
-if "%2"=="" (
+if "%1"=="" (
     set game_setting=1
 ) else (
-    set game_setting=%2
+    set game_setting=%1
 )
 
-@REM Check if there is any assignment of name of source code to wrap
-if "%3"=="" (
-    set source1=Sample
+@REM Check if there is any assignment of wrapped executable name
+if "%2"=="" (
+    set executable1=Model_1
 ) else (
-    set source1=%3
+    set executable1=%2
 )
 
 @REM Check if there is any assignment of agent to replace
-if "%4"=="" (
+if "%3"=="" (
     set agent1=1
 ) else (
-    set agent1=%4
+    set agent1=%3
+)
+
+@REM Check if there is any assignment of name of source code to wrap
+if "%4"=="" (
+    set source1=Sample
+) else (
+    set source1=%4
 )
 
 cd game_%game_setting%
